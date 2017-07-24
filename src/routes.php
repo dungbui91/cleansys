8dbd11d85d8b78af28b3085258178d4f64514ef3
dungbui91 <qdspkit@gmail.com>
2017-07-24 16:29:50 +0700

    Update name router

--- src/routes.php
+++ src/routes.php
@@ -1,17 +1,17 @@
 <?php
 
-Route::get('enableMaintainMode', 'DungBui\CleanSys\CleansysController@enableMaintainMode');
+Route::get('enableMaintainMode', 'DungBui\CleanSys\CleansysController@enableMaintainMode')->name('enableMaintainMode');
 
-Route::get('disableMaintainMode', 'DungBui\CleanSys\CleansysController@disableMaintainMode');
+Route::get('disableMaintainMode', 'DungBui\CleanSys\CleansysController@disableMaintainMode')->name('disableMaintainMode');
 
-Route::get('enableCachePage', 'DungBui\CleanSys\CleansysController@enableCachePage');
+Route::get('enableCachePage', 'DungBui\CleanSys\CleansysController@enableCachePage')->name('enableCachePage');
 
-Route::get('disableCachePage', 'DungBui\CleanSys\CleansysController@disableCachePage');
+Route::get('disableCachePage', 'DungBui\CleanSys\CleansysController@disableCachePage')->name('disableCachePage');
 
-Route::get('clearCachePage', 'DungBui\CleanSys\CleansysController@clearCachePage');
+Route::get('clearCachePage', 'DungBui\CleanSys\CleansysController@clearCachePage')->name('clearCachePage');
 
-Route::get('clearCacheView', 'DungBui\CleanSys\CleansysController@clearCacheView');
+Route::get('clearCacheView', 'DungBui\CleanSys\CleansysController@clearCacheView')->name('clearCacheView');
 
-Route::get('clearCompile', 'DungBui\CleanSys\CleansysController@clearCompile');
+Route::get('clearCompile', 'DungBui\CleanSys\CleansysController@clearCompile')->name('clearCompile');
 
-Route::get('clearAll', 'DungBui\CleanSys\CleansysController@clearAll');
+Route::get('clearAll', 'DungBui\CleanSys\CleansysController@clearAll')->name('clearAll');
