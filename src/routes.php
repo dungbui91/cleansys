37f251687cb55b1d2fe312f9b8a759db8927ab47
dungbui91 <qdspkit@gmail.com>
2017-07-24 14:24:48 +0700

    Edit routes

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,13 @@
 <?php
 
 Route::get('enableMaintainMode', 'DungBui\CleanSys\CleansysController@enableMaintainMode');
+
+Route::get('disableMaintainMode', 'DungBui\CleanSys\CleansysController@disableMaintainMode');
+
+Route::get('enableCachePage', 'DungBui\CleanSys\CleansysController@enableCachePage');
+
+Route::get('disableCachePage', 'DungBui\CleanSys\CleansysController@disableCachePage');
+
+Route::get('clearCachePage', 'DungBui\CleanSys\CleansysController@clearCachePage');
+
+Route::get('clearCacheView', 'DungBui\CleanSys\CleansysController@clearCacheView');
